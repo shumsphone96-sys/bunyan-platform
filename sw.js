b84cda60d19167a2497c050f71f05cbb6be9d264
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-24 23:04:40 +0200

    Initial BUNYAN platform upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='bunyan-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/static/styles.css','/static/app.js']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
