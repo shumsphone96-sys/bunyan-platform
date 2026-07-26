975118aca924c9fe8251d4e600c6f6d20d83fa41
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-26 08:40:01 +0200

    Refresh admin password controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='bunyan-v6-workflow';
+const C='bunyan-v7-password';
 const ASSETS=['./','./index.html','./styles.css','./workflow.css','./app.js','./workflow.js','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(C).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
