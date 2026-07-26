842118e043b151b22d64d639a5e3ef1af374179e
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-26 18:29:12 +0200

    Replace runtime patches with unified server entrypoint

--- backend/src/bootstrap.js
+++ backend/src/bootstrap.js
@@ -1,7 +1,1 @@
-import './request-config-patch.js';
-import './auth-patch.js';
-import './donation-receipt-patch.js';
-import './platform-patch.js';
-import './global-admin-patch.js';
-import './contact-patch.js';
 await import('./server-v6.js');
