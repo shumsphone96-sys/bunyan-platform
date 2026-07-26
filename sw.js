2a7bc7ace1e5cec8addcbceca27597e521d40f08
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-26 08:56:01 +0200

    Refresh cache for complete account security centre

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='bunyan-v7-password';
-const ASSETS=['./','./index.html','./styles.css','./workflow.css','./app.js','./workflow.js','./manifest.json'];
+const C='bunyan-v8-account-security';
+const ASSETS=['./','./index.html','./styles.css','./workflow.css','./auth-center.css','./app.js','./workflow.js','./auth-center.js','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(C).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
