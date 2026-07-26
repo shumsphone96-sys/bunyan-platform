4719f3b3798d03b5f4cea23a2f1052e2e6763796
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-26 09:23:42 +0200

    Refresh global governance assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='bunyan-v10-global-operations';
-const ASSETS=['./','./index.html','./styles.css','./workflow.css','./auth-center.css','./global-upgrade.css','./app.js','./workflow.js','./auth-center.js','./donation-receipt.js','./global-upgrade.js','./manifest.json'];
+const C='bunyan-v11-global-governance';
+const ASSETS=['./','./index.html','./styles.css','./workflow.css','./auth-center.css','./global-upgrade.css','./global-admin.css','./app.js','./workflow.js','./auth-center.js','./donation-receipt.js','./global-upgrade.js','./global-admin.js','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(C).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
