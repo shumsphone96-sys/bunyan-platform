14b76a3147eb9a0d9ada0572541d2cba832acb51
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-26 11:40:52 +0200

    Load contact notification routes

--- backend/src/bootstrap.js
+++ backend/src/bootstrap.js
@@ -2,4 +2,5 @@ import './auth-patch.js';
 import './donation-receipt-patch.js';
 import './platform-patch.js';
 import './global-admin-patch.js';
+import './contact-patch.js';
 await import('./server-v6.js');
