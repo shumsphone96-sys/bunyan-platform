cf8a7c310eb4b12a087bca01ddc2a74c2fe1fd57
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-27 07:54:54 +0200

    Force rollout of project cover upload interface

--- api-domain.js
+++ api-domain.js
@@ -29,7 +29,7 @@
     document.head.appendChild(script);
   };
 
-  const release='20260727-project-cover-1';
+  const release='20260727-project-cover-2';
   loadStyle(`./project-center.css?v=${release}`,'project-center-style');
   loadStyle(`./financial-center.css?v=${release}`,'financial-center-style');
   loadStyle(`./global-suite.css?v=${release}`,'global-suite-style');
