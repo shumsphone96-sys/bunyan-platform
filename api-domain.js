56cfb548e14177c8f5da91c29bc0ac2731a443e5
shumsphone96-sys <shumsphone96@gmail.com>
2026-07-28 23:46:14 +0200

    Release complete bilingual interface

--- api-domain.js
+++ api-domain.js
@@ -29,7 +29,7 @@
     document.head.appendChild(script);
   };
 
-  const release='20260728-live-public-transparency-2';
+  const release='20260728-complete-bilingual-interface-1';
   loadStyle(`./project-center.css?v=${release}`,'project-center-style');
   loadStyle(`./financial-center.css?v=${release}`,'financial-center-style');
   loadStyle(`./global-suite.css?v=${release}`,'global-suite-style');
