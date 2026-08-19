0dd55f16daa7987eea6c1324b65b0e7fcb3ef2b9
shumsphone96-sys <shumsphone96@gmail.com>
2026-08-19 22:56:15 +0200

    Bust cache for admin mobile drawer fix

--- api-domain.js
+++ api-domain.js
@@ -3,7 +3,7 @@
 
   const official='https://api.bunyan-sudan.org';
   const render='https://bunyan-api-qhkf.onrender.com';
-  const release='20260819-mobile-menu-stable-1';
+  const release='20260819-admin-mobile-drawer-fix-2';
 
   window.BUNYAN_API_ORIGINS=[official,render];
   window.BUNYAN_API_ORIGIN=official;
